efde3529b2edf0f1272598f28967a028195d5908
juanikitro <jpkitro@gmail.com>
2023-06-07 20:59:16 -0300

    init

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import path from "path";
 
 import { loadApiEndpoints } from "./controllers/api";
 
--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -1,8 +1,7 @@
 import { Application, Request, Response } from "express";
 
-
 export const loadApiEndpoints = (app: Application): void => {
 	app.get("/api", (req: Request, res: Response) => {
-		return res.status(200).send('hi!');
+		return res.status(200).send("hi!");
 	});
 };
