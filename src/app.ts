67385a88cc2f660e9194c800101a0279fb0149a6
juanikitro <jpkitro@gmail.com>
2023-06-07 20:42:20 -0300

    init

--- src/app.ts
+++ src/app.ts
@@ -11,8 +11,6 @@ app.set("port", process.env.PORT ?? 3000);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, "../public"), { maxAge: 31557600000 }));
-
 loadApiEndpoints(app);
 
 export default app;
--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -1,9 +1,8 @@
 import { Application, Request, Response } from "express";
 
-import CoursesData from "../../data/courses.json";
 
 export const loadApiEndpoints = (app: Application): void => {
 	app.get("/api", (req: Request, res: Response) => {
-		return res.status(200).send(CoursesData);
+		return res.status(200).send('hi!');
 	});
 };
