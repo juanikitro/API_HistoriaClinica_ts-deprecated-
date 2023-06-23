333d44ab9aa4010cc6b33d3f810b852811800db0
juanikitro <jpkitro@gmail.com>
2023-06-22 21:43:39 -0300

    add cache expiration time to .env

--- src/services/cacheResponse.ts
+++ src/services/cacheResponse.ts
@@ -1,8 +1,11 @@
+import dotenv from 'dotenv';
 import redisClient from '../configs/redis';
+
+dotenv.config();
 
 async function cacheResponse(key:string, value: unknown) {
   await redisClient.connect();
-  await redisClient.setex(key, 3600, JSON.stringify(value));
+  await redisClient.setex(key, process.env.CACHE_EXPIRATION_TIME ?? 3600, JSON.stringify(value));
   await redisClient.quit();
 }
 
