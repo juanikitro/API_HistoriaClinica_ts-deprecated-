d04fcadaefaa2c60fb8ff1cb198dd8874dc1e4ad
juanikitro <jpkitro@gmail.com>
2023-06-27 23:30:37 -0300

    change findPersonPregnantMinor service to findPersonPregnant and fix the max date in that

--- src/controllers/findAllPersonData.ts
+++ src/controllers/findAllPersonData.ts
@@ -8,6 +8,7 @@ import { findPersonMedicalControls } from '../services/findPersonMedicalControls
 import { findPersonSubstanceUse } from '../services/findPersonSubstanceUse';
 import { findPersonMentalProblems } from '../services/findPersonMentalProblems';
 import { findPersonPregnant } from '../services/findPersonPregnant';
+import { findPersonPregnant } from '../services/findPersonPregnant';
 import cacheResponse from '../services/cacheResponse';
 
 /**
@@ -30,7 +31,7 @@ async function findAllPersonData(req: Request, res: Response): Promise<Response>
     const personMedicalControls = await findPersonMedicalControls(persCodigo);
     const personHealthCenters = await findPersonHealthCenters(persCodigo);
     const personTurns = await findPersonTurns(persCodigo);
-    const personPregnantMinor = await findPersonPregnant(persCodigo);
+    const personPregnant = await findPersonPregnant(persCodigo);
 
     const allPersonData = {
       persona: {
@@ -44,7 +45,7 @@ async function findAllPersonData(req: Request, res: Response): Promise<Response>
       realiza_controles_medicos: personMedicalControls.realiza_controles_medicos,
       centros_de_salud_donde_se_atiende: personHealthCenters.centros_de_salud_donde_se_atiende,
       turnos: personTurns.turnos,
-      persona_embarazada_actualmente: personPregnantMinor.persona_embarazada_actualmente,
+      persona_embarazada_actualmente: personPregnant.persona_embarazada_actualmente,
     };
 
     cacheResponse(`${body.document_number}${body.document_type}${body.gender}`, allPersonData);
