e9e6581f2f3fe84b2507a910e2dbd2dc3339dd38
juanikitro <jpkitro@gmail.com>
2023-06-27 22:18:03 -0300

    add todo and null default in findPersonMedicalControls

--- src/services/findPersonMedicalControls.ts
+++ src/services/findPersonMedicalControls.ts
@@ -20,15 +20,15 @@ export async function findPersonMedicalControls(persCodigo: number): Promise<{
         TURN.turnLlegada IS NOT NULL AND
         SUES.suesDescripcion LIKE '%CONTROL%'
     ORDER BY TURN.turnCodigo DESC;`;
+    // TODO: Consultar si esta es la forma correcta de encontrar controles medicos
   const result = await pool.request()
     .input('persCodigo', String(persCodigo))
     .query(query);
 
   return {
-    // si
     value: !!result.recordset[0]?.turnCodigo,
-    ultima_fecha_de_consulta: result.recordset[0]?.turnFechaAsignado,
-    turnCodigo: result.recordset[0]?.turnCodigo,
+    ultima_fecha_de_consulta: result.recordset[0]?.turnFechaAsignado ?? null,
+    turnCodigo: result.recordset[0]?.turnCodigo ?? null,
   };
 }
 
